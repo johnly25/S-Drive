bc6164d6f648296fd98d858c56487634c21183e6
Johnny <nguyen.johnly25>
2024-11-21 21:00:57 -0800

    add sidebar

--- router/user.js
+++ router/user.js
@@ -4,13 +4,6 @@ const controller = require("../controller/controller")
 const {validateResults, validateFolder } = require("../middleware/express-validator")
 const { uploadFile } = require("../middleware/multer")
 
-// router.get("/", controller.getIndex)
-// router.get("/signup", controller.getSignup)
-// router.post("/signup", validateUser, validateResults, controller.postSignup)
-// router.get("/login", controller.getLogin)
-// router.post("/login", controller.postLogin)
-// router.get("/logout", controller.getLogout)
-
 router.get("/upload", controller.getUpload)
 router.post("/upload", uploadFile, controller.postUpload)
 router.post("/upload/create/folder", validateFolder, validateResults, controller.postFolder)
